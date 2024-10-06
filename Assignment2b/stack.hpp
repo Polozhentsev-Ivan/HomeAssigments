ed743d86a692dbe540e674e5fa54803adab16fb6
Ivan_Polozhentsev <iv.poloz.wdesd@mail.ru>
2024-10-06 13:53:04 +0300

    Created main file, stack hpp, and cpp's for my functions. Declared my class 'stack' in hpp

---
+++ Assignment2b/stack.hpp
@@ -0,0 +1,17 @@
+#ifndef STACK.HPP
+#define STACK.HPP
+#include <iostream>
+#include <string>
+class Stack {
+private:
+    int* y;
+    int top;
+public:
+    Stack(int size);
+    ~Stack();
+    void add(int x)
+    int del();
+    int take();
+};
+
+#endif
